fc4cb44da2094e793cba1e46f769daf7c6b33c06
Steeven Rodriguez <rodriguez_steven11@hotmail.com>
2019-06-28 11:58:55 -0500

    [ADD] Pantallas de temperatura y recorrido

--- AppPingui_g2/app/src/main/java/com/example/app_pingui_g2/MainActivity.java
+++ AppPingui_g2/app/src/main/java/com/example/app_pingui_g2/MainActivity.java
@@ -1,13 +1,87 @@
 package com.example.app_pingui_g2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mLogIn;
+    private EditText mUsername;
+    private EditText mPassword;
+    private RequestQueue mRequestQueue;
+    private String token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRequestQueue = Volley.newRequestQueue(this);
+
+
+        mLogIn = findViewById(R.id.logIn);
+
+        mLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mUsername = findViewById(R.id.username);
+                String username = mUsername.getText().toString();
+
+                mPassword = findViewById(R.id.password);
+                String password = mPassword.getText().toString();
+
+                String url = "https://amstdb.herokuapp.com/db/nuevo-jwt";
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("password",password);
+                JSONObject parametros = new JSONObject(params);
+
+                JsonObjectRequest request;
+                request = new JsonObjectRequest(Request.Method.POST, url, parametros, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println(response);
+                        try{
+                            token = response.getString("token");
+                            Intent intent = new Intent(MainActivity.this,MenuActivity.class);
+                            intent.putExtra("token",token);
+                            startActivity(intent);
+
+
+
+                        } catch (Exception e ){}
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+            mRequestQueue.add(request);
+
+            }
+        });
+
+
+
+
+
     }
 }
---
+++ AppPingui_g2/app/src/main/java/com/example/app_pingui_g2/MenuActivity.java
@@ -0,0 +1,53 @@
+package com.example.app_pingui_g2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+    private Button temperatura;
+    private Button recorrido;
+    private Button salir;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        final String token = getIntent().getStringExtra("token");
+
+        temperatura = findViewById(R.id.temperature);
+        temperatura.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,TemperatureActivity.class);
+                intent.putExtra("token", token);
+                startActivity(intent);
+            }
+        });
+
+        recorrido = findViewById(R.id.recorrido);
+
+        recorrido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuActivity.this,RecorridoActivity.class);
+                intent.putExtra("token", token);
+                startActivity(intent);
+            }
+        });
+
+        salir = findViewById(R.id.salir);
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
+    }
+}
