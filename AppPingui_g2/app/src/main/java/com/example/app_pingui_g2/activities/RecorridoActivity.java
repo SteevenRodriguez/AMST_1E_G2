7b140dfefa2880eaf95ebb8c1432e08df3e6c27a
Jonathan <jonathanyagual_@hotmail.com>
2019-06-29 13:07:53 -0500

    Lista de recorrido completa

--- AppPingui_g2/app/src/main/java/com/example/app_pingui_g2/activities/RecorridoActivity.java
+++ AppPingui_g2/app/src/main/java/com/example/app_pingui_g2/activities/RecorridoActivity.java
@@ -3,6 +3,7 @@ package com.example.app_pingui_g2.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -29,6 +30,7 @@ public class RecorridoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recorrido);
         token = getIntent().getStringExtra("token");
+        final LinearLayout linearLayout = findViewById(R.id.linear2);
 
         mRequestQueue = Volley.newRequestQueue(this);
 
@@ -36,46 +38,68 @@ public class RecorridoActivity extends AppCompatActivity {
         // in content do not change the layout size of the RecyclerView
         //recyclerView.setHasFixedSize(true);
 
+        for(int i=1;i<21;i++){
+            String url = "https://amstdb.herokuapp.com/db/recorrido/"+i;
 
-        JsonObjectRequest request;
-        String url = "https://amstdb.herokuapp.com/db/recorrido/1";
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    System.out.println(response);
+                    TextView camion = new TextView(RecorridoActivity.this);
+                    camion.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT));
+                    TextView begin = new TextView(RecorridoActivity.this);
+                    begin.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT));
+                    TextView end = new TextView(RecorridoActivity.this);
+                    end.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT));
+                    TextView origen = new TextView(RecorridoActivity.this);
+                    origen.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT));
+                    TextView destino = new TextView(RecorridoActivity.this);
+                    destino.setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT));
+                    try {
+                        camion.setText("Camion: "+String.valueOf(response.getInt("camion")));
+                        begin.setText("Fecha de inicio: "+response.getString("fecha_inicio").substring(0,10));
+                        end.setText("Fecha de Fin: "+response.getString("fecha_fin").substring(0,10));
+                        origen.setText("Origen: "+response.getString("origen"));
+                        destino.setText("Destino: "+response.getString("destino"));
+                        linearLayout.addView(camion);
+                        linearLayout.addView(begin);
+                        linearLayout.addView(end);
+                        linearLayout.addView(origen);
+                        linearLayout.addView(destino);
 
-        request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                System.out.println(response);
-                TextView camion = findViewById(R.id.camion);
-                TextView begin = findViewById(R.id.begin);
-                TextView end = findViewById(R.id.end);
-                TextView origen = findViewById(R.id.origen);
-                TextView destino = findViewById(R.id.destino);
-                try {
-                    camion.setText("Camion: "+String.valueOf(response.getInt("camion")));
-                    begin.setText("Fecha de inicio: "+response.getString("fecha_inicio"));
-                    end.setText("Fecha de Fin: "+response.getString("fecha_fin"));
-                    origen.setText("Origen: "+response.getString("origen"));
-                    destino.setText("Destino: "+response.getString("destino"));
 
 
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
 
-            }
-        }){
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                //return super.getHeaders();
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("Authorization", "JWT " + token); System.out.println(token);
-                return params;
-            }
-        };
-        mRequestQueue.add(request);
+                }
+            }){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    //return super.getHeaders();
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("Authorization", "JWT " + token); System.out.println(token);
+                    return params;
+                }
+            };
+            mRequestQueue.add(request);
+        }
+
 
         // use a linear layout manager
 
